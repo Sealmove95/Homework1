24fcac5657be9a23f4d164495999ff860890d487
Nikolai <sealmove95@yandex.ru>
2023-01-12 01:04:02 +0300

    Task 2

---
+++ Task 2/Program.cs
@@ -0,0 +1,10 @@
+﻿int num1 = Convert.ToInt32(Console.ReadLine());
+int num2 = Convert.ToInt32(Console.ReadLine());
+if (num1 < num2)
+  {
+    Console.WriteLine(num2);
+  }
+else
+  {
+    Console.WriteLine(num1);
+  }
